6d772f51bac67ae18eea4722bd1e237a65a9960e
unknown <ibrahimkhan.dev405@gmail.com>
2025-03-28 20:48:17 +0600

    search page show data

--- lib/datasource/dummy_data_source.dart
+++ lib/datasource/dummy_data_source.dart
@@ -90,9 +90,10 @@ class DummyDataSource extends DataSource {
   }
 
   @override
-  Future<List<BusSchedule>> getSchedulesByRouteName(String routeName) {
-    // TODO: implement getSchedulesByRouteName
-    throw UnimplementedError();
+  Future<List<BusSchedule>> getSchedulesByRouteName(String routeName) async {
+    return TempDB.tableSchedule
+        .where((schedule) => schedule.busRoute.routeName == routeName)
+        .toList();
   }
 
   @override
--- lib/providers/app_data_provider.dart
+++ lib/providers/app_data_provider.dart
@@ -1,11 +1,23 @@
 import 'package:bus_reservation_flutter_starter/datasource/data_source.dart';
 import 'package:bus_reservation_flutter_starter/datasource/dummy_data_source.dart';
+import 'package:bus_reservation_flutter_starter/models/bus_schedule.dart';
 import 'package:bus_reservation_flutter_starter/models/but_route.dart';
 import 'package:flutter/material.dart';
 
 class AppDataProvider extends ChangeNotifier {
+  List<BusSchedule> _scheduleList = [];
+
+  List<BusSchedule> get scheduleList => _scheduleList;
+
   final DataSource _dataSource = DummyDataSource();
-  Future<BusRoute?> getRouteByCityFromAndCityTo(String cityFrom, String cityTo){
+
+  Future<BusRoute?> getRouteByCityFromAndCityTo(
+      String cityFrom, String cityTo) {
     return _dataSource.getRouteByCityFromAndCityTo(cityFrom, cityTo);
+  }
+
+  void getSchedulesByRouteName(String routeName) async {
+    _scheduleList = await _dataSource.getSchedulesByRouteName(routeName);
+    notifyListeners();
   }
 }
